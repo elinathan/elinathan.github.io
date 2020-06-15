c9ab870964567d2bb1286aa28a24a0cd670a4976
eli <enathan@seas.upenn.edu>
2020-06-15 15:54:48 -0400

    add projects

--- scripts/img-gallery.js
+++ scripts/img-gallery.js
@@ -1,19 +1,29 @@
+/**
+ * Image gallery
+ * Eli Nathan
+ */
+
+
 // add class to img to signal that it should be wrapped
-$('.gallery').children().addClass('gal-img');
+if ($('.gallery:has(img)')) {
+    $('.gallery').children().addClass('gal-img');
+}
 
 // wrap img tags in tag to make adding images ot gallery easier
 if ($('.gallery:has(#noClick)')) {
     $(".gal-img").wrap("<div></div>");
+    if ('.gal-img:has(#caption)') {
+        let cap = $('#caption').attr('alt');
+    }
 } else {
     $(".gal-img").wrap("<a></a>");
 }
 
-
 $(".gallery").justifiedGallery({
     rowHeight: 300,
     margins: 24,
-    captions: false,
     cssAnimation: false,
     imagesAnimationDuration: 0,
-    lastRow: 'justify'
+    lastRow: 'justify',
 });
+
